7548b7e53b9580816198d69176e012ba6ad11d15
mayanlin <mayanlin@360.cn>
2019-10-09 17:23:16 +0800

    new datastructure

--- src/views/RelationWorkSpace/index.js
+++ src/views/RelationWorkSpace/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import CssModules from 'react-css-modules'
 
 import realationStyle from '@scss/relationWorkSpace.scss'
+
+// https://www.imooc.com/article/25369  demo
 class RelationWorkspace extends React.PureComponent {
     render() {
         return <><p styleName="first-page">first page</p></>
